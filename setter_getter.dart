52b898329de64cd7ffd019f51dec205aa2eaa871
2325811477@qq.com <2325811477@qq.com>
2023-12-28 22:11:57 +0800

    fix:fix the bug

--- setter_getter.dart
+++ setter_getter.dart
@@ -3,10 +3,31 @@ void main() {
   print(a.name);
   a.age = 100;
   print(a.age);
+  a.setname = "宝马"; // 调用 setter
+  print(a.getage); // 调用getter
+
+  a.setter("雷克萨斯");
+  a.getter();
 }
 
 class car {
-  String name;
-  int age;
-  car(this.name, this.age);
+  String _name;
+  int _age;
+  car(this._name, this._age);
+  // 这是一个setter
+  void set setname(String name) {
+    // 这个 void 可以省略
+    this._name = name;
+  }
+
+  //这个是一个getter
+  int get getage => this._age;
+  // 这是一个函数
+  void setter(String name) {
+    this._name = name;
+  }
+
+  int getter() {
+    return this._age;
+  }
 }
